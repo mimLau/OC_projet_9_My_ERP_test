782d22c84dc23c9577fbcd94c745304a66d0c6d3
DESKTOP-RRDKPNH\dell <lam99@hotmail.fr>
2020-09-17 14:13:04 +0200

    Add tests for compteComptable

---
+++ src/myerp-model/src/test/java/com/dummy/myerp/model/bean/comptabilite/CompteComptableTest.java
@@ -0,0 +1,66 @@
+package com.dummy.myerp.model.bean.comptabilite;
+
+import org.junit.jupiter.api.BeforeEach;
+import org.junit.jupiter.api.DisplayName;
+import org.junit.jupiter.api.Test;
+
+import java.util.ArrayList;
+import java.util.List;
+
+import static org.assertj.core.api.Assertions.assertThat;
+
+public class CompteComptableTest {
+
+    CompteComptable compteComptableUnderTest;
+    List<CompteComptable> compteComptableList = new ArrayList<>();
+
+    @BeforeEach
+    public void initCompteCompableList() {
+
+        compteComptableUnderTest = new CompteComptable(1, "Founisseurs");
+        compteComptableList.add(compteComptableUnderTest);
+        compteComptableUnderTest = new CompteComptable(2, "Achats");
+        compteComptableList.add(compteComptableUnderTest);
+        compteComptableUnderTest = new CompteComptable(3, "Banque");
+        compteComptableList.add(compteComptableUnderTest);
+
+
+    }
+
+    @Test
+    @DisplayName("Retrieve a compte comtable by his number.")
+    public void givenCompteComptableNb_whenGetByNumero_ifCompteExist_shouldReturnCompteComptable() {
+
+        // GIVEN
+        int compteComptableNb = 1;
+
+        // WHEN
+        CompteComptable compteComptable = CompteComptable.getByNumero(compteComptableList,compteComptableNb);
+
+        // THEN
+        assertThat(compteComptable.getNumero()).isEqualTo(compteComptableNb);
+    }
+
+    @Test
+    @DisplayName("Getting a compte comptable with a number which doesn't exist, then should return null.")
+    public void givenCompteComptableNb_whenGetByNumero_ifCompteDontExist_shouldReturnNull() {
+
+        // GIVEN
+        int compteComptableNb = 4;
+
+        // WHEN
+        CompteComptable compteComptable = CompteComptable.getByNumero(compteComptableList,compteComptableNb);
+
+        // THEN
+        assertThat(compteComptable).isEqualTo(null);
+    }
+
+    @Test
+    public void givenCompteComptableNb_whenToString_returnTheGoodSentecnce(){
+
+        CompteComptable compteComptable = new CompteComptable();
+        compteComptable.setLibelle("Achats");
+        compteComptable.setNumero(2);
+        assertThat("CompteComptable{numero=2, libelle='Achats'}").isEqualTo(compteComptable.toString());
+    }
+}
