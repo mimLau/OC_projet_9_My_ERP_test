9875a936981465c5136d8ebf0edb27153c562880
DESKTOP-RRDKPNH\dell <lam99@hotmail.fr>
2020-09-17 16:22:02 +0200

    Test of for sequence ecriture comptable

--- src/myerp-model/src/test/java/com/dummy/myerp/model/bean/comptabilite/SequenceEcritureComptableTest.java
+++ src/myerp-model/src/test/java/com/dummy/myerp/model/bean/comptabilite/SequenceEcritureComptableTest.java
@@ -8,7 +8,6 @@ import static org.assertj.core.api.Assertions.assertThat;
 public class SequenceEcritureComptableTest {
 
      SequenceEcritureComptable sequenceEcritureComptableUnderTest;
-     JournalComptable journalComptable;
 
 
     @Test
