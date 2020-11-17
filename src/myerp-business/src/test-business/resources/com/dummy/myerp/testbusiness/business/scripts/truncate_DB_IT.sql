fc0305c4d3129cafc5d051889e96493b3d600ee5
DESKTOP-RRDKPNH\dell <lam99@hotmail.fr>
2020-11-17 14:21:47 +0100

    Add scripts

--- src/myerp-business/src/test-business/resources/com/dummy/myerp/testbusiness/business/scripts/truncate_DB_IT.sql
+++
@@ -1,4 +0,0 @@
-TRUNCATE db_myerp.myerp.journal_comptable CASCADE;
-TRUNCATE db_myerp.myerp.compte_comptable CASCADE;
-TRUNCATE db_myerp.myerp.ecriture_comptable CASCADE;
-TRUNCATE db_myerp.myerp.ligne_ecriture_comptable CASCADE;
