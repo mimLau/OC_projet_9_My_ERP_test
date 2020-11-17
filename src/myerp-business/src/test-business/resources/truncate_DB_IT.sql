ec791c001a9d723a3f371af097a2f671a3dd6bc8
DESKTOP-RRDKPNH\dell <lam99@hotmail.fr>
2020-11-17 14:34:54 +0100

    Add scripts

---
+++ src/myerp-business/src/test-business/resources/truncate_DB_IT.sql
@@ -0,0 +1,4 @@
+TRUNCATE db_myerp.myerp.journal_comptable CASCADE;
+TRUNCATE db_myerp.myerp.compte_comptable CASCADE;
+TRUNCATE db_myerp.myerp.ecriture_comptable CASCADE;
+TRUNCATE db_myerp.myerp.ligne_ecriture_comptable CASCADE;
